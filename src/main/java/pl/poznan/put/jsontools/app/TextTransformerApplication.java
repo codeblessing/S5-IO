c050f4e3f704fac93db2bd32ebd9d00aee0e46fc
Jakub Kwiatkowski <jakub.m.kwiatkowski@gmail.com>
2021-12-08 14:28:32 +0100

    Feature: GitHub Actions additional setup.

--- src/main/java/pl/poznan/put/jsontools/app/TextTransformerApplication.java
+++ src/main/java/pl/poznan/put/jsontools/app/TextTransformerApplication.java
@@ -3,7 +3,7 @@ package pl.poznan.put.jsontools.app;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-
+/// Comment
 @SpringBootApplication(scanBasePackages = {"pl.poznan.put.jsontools.rest"})
 public class TextTransformerApplication {
 
