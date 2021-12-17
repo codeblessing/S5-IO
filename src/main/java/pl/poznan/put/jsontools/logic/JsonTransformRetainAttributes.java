8c0ef05b162fb22d7332d880170a831206979a31
jedrzejSmok <jedrzejsmok55@gmail.com>
2021-12-17 23:27:41 +0100

    comments

--- src/main/java/pl/poznan/put/jsontools/logic/JsonTransformRetainAttributes.java
+++ src/main/java/pl/poznan/put/jsontools/logic/JsonTransformRetainAttributes.java
@@ -6,14 +6,23 @@ import com.fasterxml.jackson.databind.node.ObjectNode;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
-
+/**
+ * Class enables to get JSON containing only certain properties.
+ */
 public class JsonTransformRetainAttributes implements JsonTransform {
     private final List<String> _attributes;
-
+    /**
+     * Class constructor specifying list of attributes to filter.
+     */
     public JsonTransformRetainAttributes(List<String> attributes) {
         this._attributes = attributes;
     }
 
+    /**
+     * It enables to enter JsonNode  that will be simplified to structure containing certain attributes.
+     * @param json JSON to be simplified
+     * @return json This is JSON containing only attributes specified by  {@link #_attributes}
+     */
     @Override
     public JsonNode execute(JsonNode json) {
         List<String> jsonAttributes = new ArrayList<>();
