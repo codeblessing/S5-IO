272218753f76d4f267bcdbc68ef57e70fe2d817f
Paweł Strzelczyk <61944576+pawelstrzelczyk@users.noreply.github.com>
2021-12-18 20:24:19 +0100

    added comments to JsonTransformFormat.java

--- src/main/java/pl/poznan/put/jsontools/logic/JsonTransformFormat.java
+++ src/main/java/pl/poznan/put/jsontools/logic/JsonTransformFormat.java
@@ -6,11 +6,24 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+/**
+ * Class enables to transform minified JSON file into JSON full form
+ */
 public class JsonTransformFormat implements JsonFinalTransform {
 
+    /**
+     * ObjectMapper to apply formatting to the JSON file
+     */
     private static final ObjectMapper mapper = new ObjectMapper();
+    /**
+     * Log errors in JsonTransformFormat class
+     */
     private static final Logger logger = LoggerFactory.getLogger(JsonTransformFormat.class);
 
+    /**
+     * @param json minified JSON file
+     * @return String prettified file (with proper formatting, e.g. spaces, tabs, newlines)
+     */
     @Override
     public String execute(JsonNode json) {
 
