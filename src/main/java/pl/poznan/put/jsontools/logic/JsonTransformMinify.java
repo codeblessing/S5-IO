d89ebc56b841fa07e0178d7dabaf28a7dffa7ab5
Martyna Kurz <martyna.kurz@student.put.poznan.pl>
2021-12-21 14:10:12 +0100

    minification function added

--- src/main/java/pl/poznan/put/jsontools/logic/JsonTransformMinify.java
+++ src/main/java/pl/poznan/put/jsontools/logic/JsonTransformMinify.java
@@ -1,15 +1,23 @@
 package pl.poznan.put.jsontools.logic;
 
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 public class JsonTransformMinify extends JsonTransformer {
     public JsonTransformMinify(JsonTransform transform) {
         super(transform);
     }
-
+    private static final ObjectMapper mapper = new ObjectMapper();
     @Override
     public String execute() {
+        try {
+            return mapper.readTree(super.execute()).toString();
+        } catch (JsonProcessingException e) {
+            e.printStackTrace();
+        }
+
         return null;
     }
 }
