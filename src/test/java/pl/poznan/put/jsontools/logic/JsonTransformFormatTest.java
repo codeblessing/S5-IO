9be70ec1bff1c6059687845baa3e5283e11c7188
Paweł Strzelczyk <61944576+pawelstrzelczyk@users.noreply.github.com>
2022-01-16 18:57:09 +0100

    fixed tests to JsonTransformFormat

--- src/test/java/pl/poznan/put/jsontools/logic/JsonTransformFormatTest.java
+++ src/test/java/pl/poznan/put/jsontools/logic/JsonTransformFormatTest.java
@@ -18,9 +18,9 @@ public class JsonTransformFormatTest {
     public void testEasyFormat() {
         String input = "{\"random\":28,\"regEx\":\"hellooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooo to you\"}";
         JsonTransformFormat transform = new JsonTransformFormat(new JsonBase(input));
-        String expected = "{\r\n" +
-                "  \"random\" : 28,\r\n" +
-                "  \"regEx\" : \"hellooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooo to you\"\r\n" +
+        String expected = "{\n" +
+                "  \"random\" : 28,\n" +
+                "  \"regEx\" : \"hellooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooo to you\"\n" +
                 "}";
 
         assertEquals(expected, transform.execute());
@@ -30,17 +30,17 @@ public class JsonTransformFormatTest {
     public void testModerateFormat() {
         String input = "{\"friends\":[{\"id\":0,\"name\":\"Dianna Madden\"},{\"id\":1,\"name\":\"Blanche Aguilar\"},{\"id\":2,\"name\":\"Lesley Jacobson\"}]}";
         JsonTransformFormat transform = new JsonTransformFormat(new JsonBase(input));
-        String expected = "{\r\n" +
-                "  \"friends\" : [ {\r\n" +
-                "    \"id\" : 0,\r\n" +
-                "    \"name\" : \"Dianna Madden\"\r\n" +
-                "  }, {\r\n" +
-                "    \"id\" : 1,\r\n" +
-                "    \"name\" : \"Blanche Aguilar\"\r\n" +
-                "  }, {\r\n" +
-                "    \"id\" : 2,\r\n" +
-                "    \"name\" : \"Lesley Jacobson\"\r\n" +
-                "  } ]\r\n" +
+        String expected = "{\n" +
+                "  \"friends\" : [ {\n" +
+                "    \"id\" : 0,\n" +
+                "    \"name\" : \"Dianna Madden\"\n" +
+                "  }, {\n" +
+                "    \"id\" : 1,\n" +
+                "    \"name\" : \"Blanche Aguilar\"\n" +
+                "  }, {\n" +
+                "    \"id\" : 2,\n" +
+                "    \"name\" : \"Lesley Jacobson\"\n" +
+                "  } ]\n" +
                 "}";
 
         assertEquals(expected, transform.execute());
