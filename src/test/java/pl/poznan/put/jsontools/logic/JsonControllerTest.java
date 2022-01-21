85e80d7b73dabe268794a8bbfc4a59abff536444
jedrzejSmok <jedrzejsmok55@gmail.com>
2022-01-21 23:35:23 +0100

    added tests

--- src/test/java/pl/poznan/put/jsontools/logic/JsonControllerTest.java
+++ src/test/java/pl/poznan/put/jsontools/logic/JsonControllerTest.java
@@ -9,6 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import pl.poznan.put.jsontools.error.JsonToolsInvalidJsonError;
 import pl.poznan.put.jsontools.rest.JsonToolsController;
 import pl.poznan.put.jsontools.rest.JsonToolsFullRequest;
 import pl.poznan.put.jsontools.rest.JsonToolsRequestTransform;
@@ -53,12 +54,74 @@ public class JsonControllerTest {
 
     }
 
+    @Test
+    public void invocationRemoveAttributesTest() throws JsonProcessingException {
+        jsonToolsController.removeAttributes(createJsonToolsSingleRequest());
+        verify(jsonTransformService, times(1)).removeAttributes(
+                any()
+        );
+    }
 
+    @Test
+    public void invocationRetainAttributesTest() throws JsonProcessingException {
+        jsonToolsController.retainAttributes(createJsonToolsSingleRequest());
+        verify(jsonTransformService, times(1)).retainAttributes(
+                any()
+        );
+    }
 
+    @Test
+    public void invocationFlattenTest() throws JsonProcessingException {
+        jsonToolsController.flatten(createJsonToolsSingleRequest());
+        verify(jsonTransformService, times(1)).flatten(
+                any()
+        );
+    }
 
+    @Test
+    public void invocationMinifyTest() throws JsonProcessingException {
+        jsonToolsController.minify(createJsonToolsSingleRequest());
+        verify(jsonTransformService, times(1)).minify(
+                any()
+        );
+    }
 
+    @Test
+    public void invocationFormatTest() throws JsonProcessingException {
+        jsonToolsController.format(createJsonToolsSingleRequest());
+        verify(jsonTransformService, times(1)).format(
+                any()
+        );
+    }
 
+    @Test
+    public void invocationSortAttributesTest() throws JsonProcessingException {
+        jsonToolsController.sortAttributes(createJsonToolsSingleRequest());
+        verify(jsonTransformService, times(1)).sortAttributes(
+                any()
+        );
+    }
 
+    @Test
+    public void invocationCountFieldsValuesTest() throws JsonProcessingException {
+        jsonToolsController.countFieldsValues(createJsonToolsSingleRequest());
+        verify(jsonTransformService, times(1)).countFieldsValues(
+                any()
+        );
+    }
+
+    @Test
+    public void invocationNeverCountFieldsValuesTest() throws JsonProcessingException {
+        jsonToolsController.sortAttributes(createJsonToolsSingleRequest());
+        verify(jsonTransformService, never()).countFieldsValues(
+                any()
+        );
+    }
+
+    @Test
+    public void throwJsonProcessingExceptionTest() throws JsonProcessingException {
+        doThrow(JsonToolsInvalidJsonError.class).when(jsonTransformService).sortAttributes(createJsonToolsSingleRequest());
+    }
 
 
     public JsonToolsFullRequest createJsonToolsFullRequest() throws JsonProcessingException {
@@ -66,7 +129,7 @@ public class JsonControllerTest {
         JsonToolsFullRequest jsonToolsFullRequest = new JsonToolsFullRequest();
 
         ObjectMapper mapper = new ObjectMapper();
-        JsonNode jsonNode = mapper.readTree("{\"random\":28,\"regEx\":\"hellooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooo to you\"}");
+        JsonNode jsonNode = mapper.readTree("\"{random\":28,\"regEx\":\"hellooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooo to you\"}");
 
         JsonToolsRequestTransform jsonToolsRequestTransform = new JsonToolsRequestTransform();
 
@@ -91,6 +154,5 @@ public class JsonControllerTest {
 
         return jsonToolsSingleRequest;
     }
-
-
+    
 }
