5a7533ee37b6fd57274aac9ae2b6f9c3885009ad
Paweł Strzelczyk <61944576+pawelstrzelczyk@users.noreply.github.com>
2022-01-25 01:25:14 +0100

    add gson dependency, cleanup

--- src/main/java/pl/poznan/put/jsontools/logic/JsonTransformDeleteNulls.java
+++ src/main/java/pl/poznan/put/jsontools/logic/JsonTransformDeleteNulls.java
@@ -1,32 +1,19 @@
 package pl.poznan.put.jsontools.logic;
 
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.JsonNode;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.node.ObjectNode;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import pl.poznan.put.jsontools.error.JsonToolsInvalidJsonError;
-
-import java.util.ArrayList;
-import java.util.List;
-
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import com.google.gson.JsonObject;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import pl.poznan.put.jsontools.error.JsonToolsInvalidJsonError;
 
 public class JsonTransformDeleteNulls extends JsonTransformer {
 
     /**
      * Class-level logger instance.
      */
-    private static final Logger _logger = LoggerFactory.getLogger(JsonTransformRemoveAttributes.class);
-    /**
-     * Object mapper to retrieve JSON structure from string.
-     */
-    private static final ObjectMapper mapper = new ObjectMapper();
-
+    private static final Logger _logger = LoggerFactory.getLogger(JsonTransformDeleteNulls.class);
 
     public JsonTransformDeleteNulls(JsonTransform transform) {
         super(transform);
@@ -40,10 +27,8 @@ public class JsonTransformDeleteNulls extends JsonTransformer {
         try {
             String json = input.replace("\"\"", "null");
             JsonObject jsonObject = new Gson().fromJson(json, JsonObject.class);
-            String s = jsonObject.toString();
             Gson gson = new GsonBuilder().create();
-            s = gson.toJson(jsonObject);
-            return s;
+            return gson.toJson(jsonObject);
         }
         catch (Exception e) {
             throw new JsonToolsInvalidJsonError(e.getMessage());
